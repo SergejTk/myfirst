1d2a48c8db2d7a5e2e2345570f6aad3aba72b50f
SergejTk <kiberbeatle@gmail.com>
2016-07-18 18:24:23 +0400

    Переделано по части Spring-а. Не закончено. Вопросы по аннотации @PropertySource - не принимает аргумент

--- src/main/java/me/tkachenko/myfirst/WorkersDAO.java
+++ src/main/java/me/tkachenko/myfirst/WorkersDAO.java
@@ -3,29 +3,28 @@ package me.tkachenko.myfirst;
 import me.tkachenko.myfirst.config.BeanConfig;
 import me.tkachenko.myfirst.config.DBReader;
 import org.hibernate.Session;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
  * Created by ִלטענטי on 15.07.2016.
  */
+@Component
+
 public class WorkersDAO {
 
-    private DBReader reader;
-    private Session session = reader.getSession();
+    private Session session;
     ApplicationContext context =
             new AnnotationConfigApplicationContext(BeanConfig.class);
-    DBReader dbReader = context.getBean(DBReader.class);
+    DBReader reader = context.getBean(DBReader.class);
 
-    @Autowired
-    public WorkersDAO(DBReader dbReader) {
-        reader = dbReader;
-    }
+
 
     public List<Worker> getAllWorkers() {
+        session = reader.getSessionFactory().getCurrentSession();
         return this.session.createQuery("SELECT * FROM  workers").list();
     }
 }
--- src/main/java/me/tkachenko/myfirst/config/BeanConfig.java
+++ src/main/java/me/tkachenko/myfirst/config/BeanConfig.java
@@ -5,24 +5,45 @@ package me.tkachenko.myfirst.config;
  */
 
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import java.util.Properties;
 
 @Configuration
+//@PropertySources( {"classpath:DBProperties.properties" })
 public class BeanConfig {
+    @Autowired
+    private Environment env;
 
     @Bean
     DBReader dbReader() {
 
         return new DBReader() {
-            public Session getSession() {
-                SessionFactory sessionFactory = null;
-                return sessionFactory.getCurrentSession();
+            @Override
+            public SessionFactory getSessionFactory() {
+                LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+                SessionFactory sessionFactory;
+                //
 
+                localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+                sessionFactory = localSessionFactoryBean.getObject();
+                return sessionFactory;
             }
+        };
+    }
 
+    Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.hbm2ddl.auto", "update");
+                setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL94Dialect");
+                setProperty("hibernate.globally_quoted_identifiers", "true");
+            }
         };
     }
 }
