d1d700806691289a0a1ee93f06c7b96f3ffccb0d
SergejTk <kiberbeatle@gmail.com>
2016-08-16 11:18:20 +0400

    remove @Transactional from WorkersDAO

--- src/main/java/me/tkachenko/myfirst/workersdao/WorkersDAO.java
+++ src/main/java/me/tkachenko/myfirst/workersdao/WorkersDAO.java
@@ -1,7 +1,6 @@
 package me.tkachenko.myfirst.workersdao;
 
 import me.tkachenko.myfirst.model.Worker;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -9,12 +8,9 @@ import java.util.List;
  * Created by alexk on 15.08.2016 22:59.
  */
 public interface WorkersDAO {
-    @Transactional(readOnly = true)
     List<Worker> getAllWorkers();
 
-    @Transactional(readOnly = true)
     List<Worker> getPartWorkers(int start, int length, String columnName, boolean isAsc);
 
-    @Transactional(readOnly = true)
     Number getTotalRow();
 }
